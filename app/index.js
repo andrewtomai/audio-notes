d409d295a0ab2644b0dec12efe19e11475930f1b
Andrew Tomai <andrew@locuslabs.com>
2019-02-28 17:48:58 -0800

    🔥 Removing code or files: i aint want redux or any dat shit ➖ Removing dependencies

--- app/index.js
+++ app/index.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
 import './app.global.css';
+import Home from './components/Home';
 
-const store = configureStore();
 
 render(
   <AppContainer>
-    <Root store={store} history={history} />
+    <Home/>
   </AppContainer>,
   document.getElementById('root')
 );
 
 if (module.hot) {
-  module.hot.accept('./containers/Root', () => {
+  module.hot.accept('./components/Home', () => {
     // eslint-disable-next-line global-require
-    const NextRoot = require('./containers/Root').default;
+    const NextRoot = require('./components/Home').default;
     render(
       <AppContainer>
-        <NextRoot store={store} history={history} />
+        <NextRoot />
       </AppContainer>,
       document.getElementById('root')
     );
